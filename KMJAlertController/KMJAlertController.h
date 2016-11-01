4dc6fbdd2bdd01f3348538090fc7014e37b4654e
komaji <cy11121@shibaura-it.ac.jp>
2016-11-01 23:09:43 +0900

    git init

---
+++ KMJAlertController/KMJAlertController.h
@@ -0,0 +1,19 @@
+//
+//  KMJAlertController.h
+//  KMJAlertController
+//
+//  Created by KojimaTatsuya on 2016/11/01.
+//  Copyright © 2016年 komaji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KMJAlertController.
+FOUNDATION_EXPORT double KMJAlertControllerVersionNumber;
+
+//! Project version string for KMJAlertController.
+FOUNDATION_EXPORT const unsigned char KMJAlertControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KMJAlertController/PublicHeader.h>
+
+
---
+++ KMJAlertControllerDemo/KMJAlertControllerDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KMJAlertControllerDemo
+//
+//  Created by KojimaTatsuya on 2016/11/01.
+//  Copyright © 2016年 komaji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
