5c7b73a1e5a18c1a959aa0a44fbdbac07155c521
komaji <cy11121@shibaura-it.ac.jp>
2016-11-01 23:35:49 +0900

    create KMJAlertController.swift

---
+++ KMJAlertController/KMJAlertController.swift
@@ -0,0 +1,9 @@
+//
+//  KMJAlertController.swift
+//  KMJAlertController
+//
+//  Created by KojimaTatsuya on 2016/11/01.
+//  Copyright © 2016年 komaji. All rights reserved.
+//
+
+import Foundation
